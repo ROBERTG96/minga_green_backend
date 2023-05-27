0656e7c90a1fdc5126f5e8401d034e0886119641
Robert Michael Gonzalez Nuñez <rmgonzalezprostudio@gmail.com>
2023-05-26 19:03:35 -0500

    minga-10 - EndPoint - Controller

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import authorsRouter from "./authors.js";
 import authRouter from "./auth.js";
 import mangasRouter from "./mangas.js";
 import chaptersRouter from "./chapters.js";
+import categoriesRouter from "./categories.js";
+
 
 
 const indexRouter = Router();
@@ -11,5 +13,7 @@ indexRouter.use("/authors", authorsRouter);
 indexRouter.use("/auth", authRouter);
 indexRouter.use("/mangas", mangasRouter);
 indexRouter.use("/chapters", chaptersRouter);
+indexRouter.use("/categories", categoriesRouter);
+
 
 export default indexRouter;
