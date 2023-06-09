762609866633cb9e71a34b6142d87d6b86b8d136
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-06-08 19:24:01 -0500

    MongodbUri Changed (#9)
    * Modelo User y Script Users
    
    * Creacion de modelo Category - Script Category
    
    * Creacion modelo manga - chapter - Scripts
    
    * Manga
    
    * minga-09 router - controllers
    
    * minga-10 - EndPoint - Controller
    
    * update mongo_uri
    
    ---------
    
    Co-authored-by: Robert Michael Gonzalez Nuñez <rmgonzalezprostudio@gmail.com>

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function connect_db() {
   try {
    const db = await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database connection established:", db.connection.port);
+    console.log("Database connection established:", db.connection.db.databaseName);
   } catch (error) {
     console.error("Error connecting to Mongoose", error);
   }
